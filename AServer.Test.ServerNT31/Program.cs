de3eebc18f5f083f1df267e47f32f36edfd04a75
kklldog <kklldog@qq.com>
2021-06-02 22:05:22 +0800

    add ntcore31 test

---
+++ AServer.Test.ServerNT31/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AServer.Test.ServerNT31
+{
+    class Program
+    {
+        async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var server = new Agile.AServer.Server();
+
+            server.AddHandler(new Agile.AServer.HttpHandler() { 
+                Method = "GET",
+                Path = "/api/doget",
+                Handler = (req, resp) => {
+                    return resp.Write("doget");
+                }
+            });
+
+            server.ShowLog = true;
+            server.SetIP("127.0.0.1").SetPort(6000);
+            server.Run();
+
+            await Task.Delay(-1);
+        }
+    }
+}
